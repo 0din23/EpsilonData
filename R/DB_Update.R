41a24b87aa31112ff9a64a4b5b59636d55c6063a
Niklas Knecht <knecht.n@gmx.de>
2022-05-24 18:49:21 +0200

    added an offline testfile; started building DB_Update

--- R/DB_Update.R
+++ R/DB_Update.R
@@ -11,16 +11,18 @@ DB_Update(DB_NAME, DB_DIR, DB_TC){
   # innit ----------------------------------------------------------------------
 
   # Load Ticker Catalogue and check if something is new
-  tc <- readxl::read_xlsx(DB_TC)
-  tc_old <-
-
+  mData_old <- dplyr::tbl(conn, "Update_TABLE") %>%
+    data.frame()
+  mData_new <- readxl::read_xlsx(DB_TC)
 
   # Connect to DB
   db_file <-paste0(DB_DIR, "/", DB_NAME, ".db")
   conn <- RSQLite::dbConnect(RSQLite::SQLite(),db_file)
 
   # Get Update Table
-  update_table <-
+  update_table <- dplyr::tbl(conn, "Update_TABLE") %>%
+    data.frame()
+
   # Metadata -------------------------------------------------------------------
 
   # Update Table ---------------------------------------------------------------
