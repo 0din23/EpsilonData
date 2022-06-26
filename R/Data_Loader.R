63c69707d1063b8406154a69fb729a5218c164c6
Niklas Knecht <knecht.n@gmx.de>
2022-06-26 18:30:37 +0200

    Aded prototype for the new Data loader

--- R/Data_Loader.R
+++ R/Data_Loader.R
@@ -1,18 +1,16 @@
 Epsilon_Data_Loader <- function(ticker, db_type, MacroDB = MacroDB_FILE,
                                 StockDB = StockDB_FILE){
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  if(db_type == "stock"){
+    conn <- RSQLite::dbConnect(RSQLite::SQLite(), StockDB)
+    data <- dplyr::tbl(conn, "Update_TABLE") %>%
+      data.frame() %>%
+      filter(symbol %in% ticker)
+  } else if(db_type == "macro"){
+    conn <- RSQLite::dbConnect(RSQLite::SQLite(), MacroDB)
+    data <- dplyr::tbl(conn, "Update_TABLE") %>%
+      data.frame() %>%
+      filter(symbol %in% ticker)
+  }
+  return(data)
 }
