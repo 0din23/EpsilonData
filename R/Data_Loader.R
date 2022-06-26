b9f1463628c5e6c36c7660b45fc5499cfd48ef17
Niklas Knecht <knecht.n@gmx.de>
2022-06-26 18:25:21 +0200

    New Update.

--- R/Data_Loader.R
+++ R/Data_Loader.R
@@ -1,9 +1,18 @@
+Epsilon_Data_Loader <- function(ticker, db_type, MacroDB = MacroDB_FILE,
+                                StockDB = StockDB_FILE){
 
 
 
 
 
-Epsilon_Data_Loader <- function(ticker, type){
+
+
+
+
+
+
+
+
 
 
 }
