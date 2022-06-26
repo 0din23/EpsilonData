bab6f0d191d4dec675863fa620582a61948128f4
Niklas Knecht <knecht.n@gmx.de>
2022-06-26 18:50:49 +0200

    update des Loaders

--- R/Data_Loader.R
+++ R/Data_Loader.R
@@ -4,13 +4,13 @@ Epsilon_Data_Loader <- function(ticker, db_type, MacroDB = MacroDB_FILE,
   if(db_type == "stock"){
     conn <- RSQLite::dbConnect(RSQLite::SQLite(), StockDB)
     data <- dplyr::tbl(conn, "StockTS_TABLE") %>%
-      data.frame() %>%
-      filter(symbol %in% ticker)
+      filter(symbol %in% ticker) %>%
+      data.frame()
   } else if(db_type == "macro"){
     conn <- RSQLite::dbConnect(RSQLite::SQLite(), MacroDB)
     data <- dplyr::tbl(conn, "Indicator_TS") %>%
-      data.frame() %>%
-      filter(symbol %in% ticker)
+      filter(symbol %in% ticker) %>%
+      data.frame()
   }
   return(data)
 }
